62745d70706ce2f4fc548ad8346c5fc889417ad9
Jasmin <jasminferolino083@gmail.com>
2024-09-24 17:10:11 +0800

    update script.ts

---
+++ script.ts
@@ -0,0 +1,43 @@
+enum BRAND{
+    SAMSUNG = "Samsung",
+    SONY = "Sony",
+    LG = "LG",
+    TCL = "TLC",
+    PANASONIC = "Panasonic",
+}
+
+type Item = {
+    brand: string;
+    color: string;
+    screen_size: string;
+    price: number;
+    print(): void;
+};
+
+const Television: Item = {
+    brand: "Samsung",
+    color: "Silver",
+    screen_size: "28",
+    price: 35000,
+    print: function (): void {
+        console.log(`I wanna buy a ${this.brand}, since it is
+        a know local brand.
+        I want it to be ${this.color} color, since I like a cool and aesthetic look.
+        I also want it to be ${this.screen_size} inches since I wanna see and experience the whole thing.
+        My budget is around ${this.price}. `);
+    },
+};
+
+const OVEN: Item = {
+    brand: "Sony",
+    color: "Black",
+    screen_size: "68",
+    price: 40000,
+    print: function (): void {
+        console.log(`I wanna buy a ${this.brand}, since it is
+        a know local brand.
+        I want it to be ${this.color} color, since I like a cool and aesthetic look.
+        I also want it to be ${this.screen_size} inches since I wanna see and experience the whole thing.
+        My budget is around ${this.price}. `);
+    },
+};

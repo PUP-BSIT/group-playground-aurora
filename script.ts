23162835ac6c28aec8d36cc881bf01bf74b724d8
ILAGAN, ANDREI JIREH M. <ilaganandreijireh@gmail.com>
2024-09-24 16:55:14 +0800

    add new message in script.js

--- script.ts
+++ script.ts
@@ -14,3 +14,16 @@ type Item = {
     print(): void;
 };
 
+const Television: Item = {
+    brand: "Samsung",
+    color: "Silver",
+    screen_size: "28",
+    price: 35000,
+    print: function (): void {
+        console.log(`I wanna buy a ${this.brand}, since it is
+        a know local brand.
+        I want it to be ${this.color} color, since I like a cool and aesthetic look.
+        I also want it to be ${this.screen_size} inches since I wanna see and experience the whole thing.
+        My budget is around ${this.price}. `);
+    },
+};
